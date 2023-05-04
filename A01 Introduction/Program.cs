74a694bb344d82a63d671130d333358d09185d1d
RAdesign <radesign@op.pl>
2023-05-04 11:16:08 +0200

    Dodaj pliki projektów.

---
+++ A01 Introduction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string storageString = "some text";
+            int numericVar = 13;
+            Console.WriteLine(storageString);
+            Console.WriteLine(numericVar);
+            Console.WriteLine("hello");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ A03 user input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A03_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string userName = Console.ReadLine();
+            userName = userName.ToUpper();
+            Console.WriteLine("Please eneter your age");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            int userAgeInYear = userAge + 1;
+            
+
+            Console.WriteLine(userName);
+            Console.WriteLine("UserAge in a year is" + userAgeInYear);
+            Console.ReadKey();
+        }
+    }
+}
