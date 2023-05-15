6421861ac4a40e15271d0de284d6a4bb9d975331
mr-craftsman <radesign@op.pl>
2023-05-15 08:59:15 +0200

    broken pc update

---
+++ Solution2/B13EXjaggedArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B13EXjaggedArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "http://tomaszles.pl/wp-content/uploads/2023/05/volleyball_players.csv";
+
+            WebClient wc = new WebClient();
+            string data = wc.DownloadString(path);
+
+            string[] separator = new string[1] { "\r\n" };
+            string[] rows = data.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            int numberOfRows = rows.Length;
+            // int numberOfCollumns = rows[0].Split(';').Length;
+
+            string[][] table = new string[numberOfRows][];
+
+            for (int i = 0; i < numberOfRows; i++)
+                table[i] = rows[i].Split(';');
+
+            //foreach (var row in table)
+            //    Console.WriteLine(string.Join(" ", row));
+
+            foreach (var row in table)
+            {
+                foreach (var cell in row)
+                    Console.Write(cell + " ");
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
